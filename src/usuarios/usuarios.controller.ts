33ef6ad965222e164882a152420ead16a7d04883
DiegoPaezJ <49296914+DiegoPaezJ@users.noreply.github.com>
2023-11-29 23:12:46 -0300

    Update usuarios.controller.ts

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -15,7 +15,7 @@ import { Roles } from 'src/auth/decorators/roles.decorator';
 
 // Auth(Role.USUARIO) se declara a que acceso tiene cada uno de los roles
 // @Auth(Role.ADMIN)
-@Auth(Role.USUARIO)
+// @Auth(Role.USUARIO)
 @Controller('usuarios')
 export class UsuariosController {
 
